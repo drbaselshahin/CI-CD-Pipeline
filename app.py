0a3fbe35a182efee69678ad47e3abe1fdcb8a992
drbaselshahin <dr.basel.shahin@gmail.com>
2026-04-07 10:52:18 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print ("Hello CI/CD")
+from math_ops import add, multiply
+
+print("===== CI/CD MULTI-FILE PROJECT =====")
+
+a = 10
+b = 5
+
+sum_result = add(a, b)
+mul_result = multiply(a, b)
+
+print("addition", sum_result)
+print(“multiplication", mul_result)
